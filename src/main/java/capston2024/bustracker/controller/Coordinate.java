466b3902e61ccab6fef5d115d3315118fbdda214
taehun2123 <pyoneng_@naver.com>
2024-06-30 00:30:12 +0900

    squash! Controller Return 객체화

--- src/main/java/capston2024/bustracker/controller/Coordinate.java
+++ src/main/java/capston2024/bustracker/controller/Coordinate.java
@@ -1,40 +1,32 @@
 package capston2024.bustracker.controller;
 
-import capston2024.bustracker.model.BusCoordinateModel;
-import capston2024.bustracker.model.CoordinateModel;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 정류장 좌표 전송
  */
 @RestController
 public class Coordinate {
-
-    private final ArrayList<CoordinateModel> BUS_STOP_MODEL = new ArrayList<>();
-    private final ArrayList<CoordinateModel> MY_BUS_STOP_MODEL = new ArrayList<>();
-    private final ArrayList<BusCoordinateModel> BUS_MODEL = new ArrayList<>();
-
     @PostMapping("/api/allBusStopCoordinate") // Post (학교 UID)
-    public List<CoordinateModel> getBusStopCoordinate(@RequestParam("uid") String uid, Model model){
-        //대충 데이터베이스에서 UID로 정류장 콜렉션 정보를 모델로 받아오는 작업(객체반환) - 아래는 예시
-        BUS_STOP_MODEL.add(new CoordinateModel("test", 1234.241, 241.241));
+    public Object getBusStopCoordinate(Model model){
+        //대충 데이터베이스에서 정류장 위치 정보를 모델로 받아오는 작업(객체반환)
+        model.addAttribute("data", "hello!!");
         //반환 값은 해당 모델
-        return BUS_STOP_MODEL;
+        return "hello";
     }
 
     @PostMapping("/api/myBusStopCoorinate")
-    public List<CoordinateModel> getMyBusStopCoordinate(Model model){
+    public Object getMyBusStopCoordinate(Model model){
         //데이터베이스에서 해당 유저 정보와 일치한 유저의 즐겨찾기 정류장 목록 조회(객체반환)
-        return MY_BUS_STOP_MODEL;
+        return "12.214, 124.124";
     }
 
     @PostMapping("/api/busCoordinate")
-    public List<BusCoordinateModel> getBusCoordinate(Model model){
+    public Object getBusCoordinate(Model model){
         //데이터베이스에서 해당 학교정보의 uid를 가지고 해당 버스의 위치를 전송
-        return BUS_MODEL;
+        return "123123";
     }
 }
