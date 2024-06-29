d59cb65c3cb164db62134614bebaff69282bef3b
taehun2123 <pyoneng_@naver.com>
2024-06-30 00:34:07 +0900

    Controller Return 객체화

--- src/main/java/capston2024/bustracker/controller/AuthVerify.java
+++ src/main/java/capston2024/bustracker/controller/AuthVerify.java
@@ -2,7 +2,6 @@ package capston2024.bustracker.controller;
 
 import capston2024.bustracker.model.AuthModel;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
