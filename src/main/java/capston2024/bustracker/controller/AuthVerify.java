63be4cf01afed762410506a27388b906bc267db0
taehun2123 <pyoneng_@naver.com>
2024-06-30 00:24:35 +0900

    Controller Return 객체화

--- src/main/java/capston2024/bustracker/controller/AuthVerify.java
+++ src/main/java/capston2024/bustracker/controller/AuthVerify.java
@@ -1,17 +1,19 @@
 package capston2024.bustracker.controller;
 
+import capston2024.bustracker.model.AuthModel;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 계정 정보 유효성 검사
  */
-@RestController
+@RestController //@Controller + @ResponseBody
 public class AuthVerify {
     @PostMapping("/api/authVerify")
-    public boolean getAuthVerify(Model model){
+    public boolean getAuthVerify(@RequestParam("id") String id, @RequestParam("pw") String pw, Model model){
         //대충 모델과 정보 일치 유무를 판단해서 일치하면 true/false를 판단하는 로직
         return true;
     }
@@ -24,7 +26,7 @@ public class AuthVerify {
     }
 
     @PostMapping("/api/authInfo")
-    public Object getAuthInfo(Model model){
-        return "계정정보";
+    public AuthModel getAuthInfo(Model model){
+        return new AuthModel();
     }
 }
--- src/main/java/capston2024/bustracker/controller/Coordinate.java
+++ src/main/java/capston2024/bustracker/controller/Coordinate.java
@@ -1,32 +1,40 @@
 package capston2024.bustracker.controller;
 
-import org.springframework.stereotype.Controller;
+import capston2024.bustracker.model.BusCoordinateModel;
+import capston2024.bustracker.model.CoordinateModel;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 정류장 좌표 전송
  */
 @RestController
 public class Coordinate {
+
+    private final ArrayList<CoordinateModel> BUS_STOP_MODEL = new ArrayList<>();
+    private final ArrayList<CoordinateModel> MY_BUS_STOP_MODEL = new ArrayList<>();
+    private final ArrayList<BusCoordinateModel> BUS_MODEL = new ArrayList<>();
+
     @PostMapping("/api/allBusStopCoordinate") // Post (학교 UID)
-    public Object getBusStopCoordinate(Model model){
-        //대충 데이터베이스에서 정류장 위치 정보를 모델로 받아오는 작업(객체반환)
-        model.addAttribute("data", "hello!!");
+    public List<CoordinateModel> getBusStopCoordinate(@RequestParam("uid") String uid, Model model){
+        //대충 데이터베이스에서 UID로 정류장 콜렉션 정보를 모델로 받아오는 작업(객체반환) - 아래는 예시
+        BUS_STOP_MODEL.add(new CoordinateModel("test", 1234.241, 241.241));
         //반환 값은 해당 모델
-        return "hello";
+        return BUS_STOP_MODEL;
     }
 
     @PostMapping("/api/myBusStopCoorinate")
-    public Object getMyBusStopCoordinate(Model model){
+    public List<CoordinateModel> getMyBusStopCoordinate(Model model){
         //데이터베이스에서 해당 유저 정보와 일치한 유저의 즐겨찾기 정류장 목록 조회(객체반환)
-        return "12.214, 124.124";
+        return MY_BUS_STOP_MODEL;
     }
 
     @PostMapping("/api/busCoordinate")
-    public Object getBusCoordinate(Model model){
+    public List<BusCoordinateModel> getBusCoordinate(Model model){
         //데이터베이스에서 해당 학교정보의 uid를 가지고 해당 버스의 위치를 전송
-        return "123123";
+        return BUS_MODEL;
     }
 }
