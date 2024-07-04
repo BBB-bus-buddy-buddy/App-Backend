205606404a7e368785b0d59e6f7056a098ea52fd
jsum01 <djadjawltjr@gmail.com>
2024-07-04 21:28:33 +0900

    [UDATAE] adapt env.properties

--- src/main/java/capston2024/bustracker/BustrackerApplication.java
+++ src/main/java/capston2024/bustracker/BustrackerApplication.java
@@ -2,8 +2,10 @@ package capston2024.bustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpasth:env.properties")
 public class BustrackerApplication {
 
 	public static void main(String[] args) {
