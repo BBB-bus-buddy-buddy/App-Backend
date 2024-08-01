b56a3542f21b7915ae57b4184ed13efc0cbb289e
taehun2123 <pyoneng_@naver.com>
2024-08-01 16:25:44 +0900

    Autowired

--- src/main/java/capston2024/bustracker/controller/AuthController.java
+++ src/main/java/capston2024/bustracker/controller/AuthController.java
@@ -2,6 +2,7 @@ package capston2024.bustracker.controller;
 
 import capston2024.bustracker.domain.Auth;
 import capston2024.bustracker.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     LoginService loginService;
 
+    @Autowired
     public AuthController(LoginService loginService) {
         this.loginService = loginService;
     }
