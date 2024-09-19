4ac83234275af39a972f8a2ac45c4e5216037521
jsum01 <djadjawltjr@gmail.com>
2024-09-19 17:32:31 +0900

    Update: 정류장 검색(정류장명 활용) API

--- src/main/java/capston2024/bustracker/controller/StationController.java
+++ src/main/java/capston2024/bustracker/controller/StationController.java
@@ -1,43 +1,45 @@
 package capston2024.bustracker.controller;
 
 import capston2024.bustracker.domain.Station;
-import capston2024.bustracker.service.StationService;
+import capston2024.bustracker.service.StationFindService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @Slf4j
 public class StationController {
 
-    @Autowired
-    private StationService stationService;
+    private final StationFindService stationFindService;
+    public StationController(StationFindService stationFindService) {
+        this.stationFindService = stationFindService;
+    }
 
-    // 특정 정류장 조회
+    // 정류장 이름 or ID로 조회
     @GetMapping("/station")
-    public ResponseEntity<Station> getStation(@RequestParam String name) {
+    public ResponseEntity<List<Station>> getStation(@RequestParam String name) {
         log.info("가져 올 버스정류장의 아이디: {}", name);
-        Optional<Station> station = stationService.getStation(name);
-
-        if (station.isEmpty()) {
-            log.warn("Station not found with name: {}", name);
+        List<Station> stations = stationFindService.getStationByName(name);
+        if (stations.isEmpty()) {
+            log.warn("해당 정류장을 찾을 수 없습니다: {}", name);
         }
-
-        return ResponseEntity.ok(station.get()); // 좌표 리스트 반환
+        log.info("조회된 정류장 수: {}", stations.size());  // 가져온 정류장 개수를 출력
+        return ResponseEntity.ok(stations);
     }
 
     // 모든 정류장 조회
     @GetMapping("/station/all")
-    public List<Station> getAllStation() {
+    public ResponseEntity<List<Station>> getAllStation() {
         log.info("클라이언트 요청: 모든 정류장 조회");
-        List<Station> stations = stationService.getAllStations();
+        List<Station> stations = stationFindService.getAllStations();
+        if (stations.isEmpty()) {
+            log.warn("정류장을 찾을 수 없습니다");
+        }
         log.info("조회된 정류장 수: {}", stations.size());  // 가져온 정류장 개수를 출력
-        return stations;
+        return ResponseEntity.ok(stations);
     }
 }
--- src/main/java/capston2024/bustracker/service/StationFindService.java
+++ src/main/java/capston2024/bustracker/service/StationFindService.java
@@ -2,27 +2,27 @@ package capston2024.bustracker.service;
 
 import capston2024.bustracker.domain.Station;
 import capston2024.bustracker.repository.StationRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
-public class StationService {
+@AllArgsConstructor
+@NoArgsConstructor
+public class StationFindService {
 
-    private final StationRepository stationRepository;
-
-    // 생성자 주입 방식
-    public StationService(StationRepository stationRepository) {
-        this.stationRepository = stationRepository;
-    }
+    @Autowired
+    private StationRepository stationRepository;
 
     // 특정 name에 해당하는 정류장의 정보를 반환
-    public Optional<Station> getStation(String name) {
+    public List<Station> getStationByName(String name) {
         log.info("{} 정류장을 찾는 중입니다....", name);
-        return stationRepository.findByName(name);
+        return stationRepository.findByNameContaining(name);
     }
 
     // 모든 정류장을 가져옴
