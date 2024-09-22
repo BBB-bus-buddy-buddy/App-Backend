9eed67e0601c2115ad239b7cf71f8df6d7912dd5
jsum01 <djadjawltjr@gmail.com>
2024-09-22 14:49:26 +0900

    Update: 도착시간 api

--- src/main/java/capston2024/bustracker/controller/KakaoApiController.java
+++ src/main/java/capston2024/bustracker/controller/KakaoApiController.java
@@ -2,6 +2,8 @@ package capston2024.bustracker.controller;
 
 import capston2024.bustracker.service.KakaoApiService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,15 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class KakaoApiController {
 
     private final KakaoApiService kakaoApiService;
 
-    @GetMapping("/directions")
-    public ResponseEntity<String> getDirections(
+    @GetMapping("/arrival-time")
+    public ResponseEntity<String> getArrivalTime(
             @RequestParam String origin,
             @RequestParam String destination) {
-        String response = kakaoApiService.getDirections(origin, destination);
-        return ResponseEntity.ok(response);
+        log.info("도착 예정 시간을 요청 받았습니다. 출발지: {}, 목적지: {}", origin, destination);
+        Integer arrivalTimeInSeconds = kakaoApiService.getArrivalTime(origin, destination);
+
+        if (arrivalTimeInSeconds == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("도착 예정 시간을 불러오지 못했습니다.");
+        }
+
+        int minutes = arrivalTimeInSeconds / 60;
+        int seconds = arrivalTimeInSeconds % 60;
+
+        String responseMessage = String.format("도착 예정 시간: %d분 %d초", minutes, seconds);
+        return ResponseEntity.ok(responseMessage);
     }
 }
--- src/main/java/capston2024/bustracker/service/KakaoApiService.java
+++ src/main/java/capston2024/bustracker/service/KakaoApiService.java
@@ -9,6 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Map;
+
+// API 문서: https://developers.kakaomobility.com/docs/navi-api/directions/
 @Service
 @Slf4j
 public class KakaoApiService {
@@ -16,7 +20,13 @@ public class KakaoApiService {
     @Value("${kakao-rest-api-key}")
     private String REST_API_KEY;
 
-    public String getDirections(String origin, String destination) {
+    /**
+     * 단일 경유지 API
+     * @param origin
+     * @param destination
+     * @return
+     */
+    public Integer getArrivalTime(String origin, String destination) {
         String url = "https://apis-navi.kakaomobility.com/v1/directions"
                 + "?origin=" + origin
                 + "&destination=" + destination
@@ -26,18 +36,30 @@ public class KakaoApiService {
                 + "&car_hipass=false"
                 + "&alternatives=false"
                 + "&road_details=false";
-
         // 헤더에 API Key 추가
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "KakaoAK " + REST_API_KEY);
 
         HttpEntity<String> entity = new HttpEntity<>(headers);
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.exchange(
+        ResponseEntity<Map> response = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
                 entity,
-                String.class);
-        return response.getBody();// 응답 내용 반환
+                Map.class);
+        // 응답 본문에서 도착 시간을 추출
+        Map<String, Object> body = response.getBody();
+        if (body != null) {
+            List<Map<String, Object>> routes = (List<Map<String, Object>>) body.get("routes");
+            if (!routes.isEmpty()) {
+                Map<String, Object> summary = (Map<String, Object>) routes.get(0).get("summary");
+                return (Integer) summary.get("duration"); // 도착 예정 시간 (초 단위)
+            }
+        }
+        return null; // 도착 시간이 없을 때 null 반환
+    }
+
+    public Integer getMultiwayArrivalTime(){
+        return 0;
     }
 }
