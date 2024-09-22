041e64d8a3568c4bcd697a1d39edc1811d60fcbe
taehun2123 <pyoneng_@naver.com>
2024-09-22 16:23:49 +0900

    Update: CSV Sensor Handler Added

---
+++ src/main/java/capston2024/bustracker/config/WebSocketCsvConfig.java
@@ -0,0 +1,23 @@
+package capston2024.bustracker.config;
+
+import capston2024.bustracker.handler.BusLocationWebSocketCsvHandler;
+import capston2024.bustracker.handler.BusLocationWebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketCsvConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private BusLocationWebSocketCsvHandler busLocationWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(busLocationWebSocketHandler, "/bus-location")
+                .setAllowedOrigins("*");
+    }
+}
