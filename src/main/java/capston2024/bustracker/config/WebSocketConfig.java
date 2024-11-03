83d7c7a8a18b5d6c014c938a94857ef26f2e93ce
taehun2123 <pyoneng_@naver.com>
2024-11-03 14:07:52 +0900

    Update: OriginFix

--- src/main/java/capston2024/bustracker/config/WebSocketConfig.java
+++ src/main/java/capston2024/bustracker/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(busLocationWebSocketHandler, "/arduino")
-                .setAllowedOriginPatterns("*");  // allowedOrigins 대신 allowedOriginPatterns 사용
+                .setAllowedOrigins("*");
     }
 }
