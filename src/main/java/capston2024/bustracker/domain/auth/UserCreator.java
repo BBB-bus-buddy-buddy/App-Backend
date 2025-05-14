e50aa470a76e1443f43131124befd7cf14e40df5
jsum01 <djadjawltjr@gmail.com>
2025-05-14 22:46:12 +0900

    feat: update UserCreator(빌더에 organizationId("") 추가)

--- src/main/java/capston2024/bustracker/domain/auth/UserCreator.java
+++ src/main/java/capston2024/bustracker/domain/auth/UserCreator.java
@@ -24,6 +24,7 @@ public class UserCreator {
                 .email(attributes.getEmail())
                 .myStations(list)
                 .role(Role.GUEST)
+                .organizationId("")
                 .build();
     }
 }
