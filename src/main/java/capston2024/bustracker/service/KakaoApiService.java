8848a595c39f25a44a7f5b1ee71c606bef80ebb0
taehun2123 <pyoneng_@naver.com>
2025-06-16 12:26:45 +0900

    update: 테스트 위도, 경도값의 잘못된 세팅으로 인한 카카오 API 위도, 경도 위치 변경

--- src/main/java/capston2024/bustracker/service/KakaoApiService.java
+++ src/main/java/capston2024/bustracker/service/KakaoApiService.java
@@ -122,15 +122,15 @@ public class KakaoApiService {
     private KakaoDirectionsResponse requestRouteEstimate(Bus bus, Station targetStation, List<Station> waypoints) {
         KakaoDirectionsRequest request = KakaoDirectionsRequest.builder()
                 .origin(new KakaoPoint(
-                        bus.getLocation().getY(), // longitude
-                        bus.getLocation().getX())) // latitude
+                        bus.getLocation().getX(), // 경도
+                        bus.getLocation().getY())) // 위도
                 .destination(new KakaoPoint(
-                        targetStation.getLocation().getY(), // longitude
-                        targetStation.getLocation().getX())) // latitude
+                        targetStation.getLocation().getX(), // longitude
+                        targetStation.getLocation().getY())) // latitude
                 .waypoints(waypoints.stream()
                         .map(station -> new KakaoPoint(
-                                station.getLocation().getY(), // longitude
-                                station.getLocation().getX())) // latitude
+                                station.getLocation().getX(), // longitude
+                                station.getLocation().getY())) // latitude
                         .collect(Collectors.toList()))
                 .priority("RECOMMEND")
                 .build();
